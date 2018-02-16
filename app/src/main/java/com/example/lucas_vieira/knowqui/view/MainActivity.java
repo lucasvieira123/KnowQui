e8ff2db7a37056de3b3e9f7d13d7700990bed4e5
lucas-vieira <lucas.vieira@aluno.uece.br>
2018-02-15 23:24:45 -0300

    alteracoes

--- app/src/main/java/com/example/lucas_vieira/knowqui/view/MainActivity.java
+++ app/src/main/java/com/example/lucas_vieira/knowqui/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.lucas_vieira.knowqui;
+package com.example.lucas_vieira.knowqui.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.lucas_vieira.knowqui.R;
 
 public class MainActivity extends AppCompatActivity {
 
