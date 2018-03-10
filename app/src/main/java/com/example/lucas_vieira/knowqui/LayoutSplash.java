d599309bc4f925877b8bd1912614814dc3c2a620
jp <joaopedro980804@gmail.com>
2018-03-10 01:06:37 -0300

    Termino Tela Cadastro - Inicio Tela Splash

---
+++ app/src/main/java/com/example/lucas_vieira/knowqui/LayoutSplash.java
@@ -0,0 +1,17 @@
+package com.example.lucas_vieira.knowqui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LayoutSplash extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_splash);
+
+
+    }
+}
