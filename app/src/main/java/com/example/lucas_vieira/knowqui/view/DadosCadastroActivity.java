5515e9e297182256bbe5397df8d2c77630fe873c
jp <joaopedro980804@gmail.com>
2018-03-11 23:09:48 -0300

    Validação dos EditText

--- app/src/main/java/com/example/lucas_vieira/knowqui/view/DadosCadastroActivity.java
+++ app/src/main/java/com/example/lucas_vieira/knowqui/view/DadosCadastroActivity.java
@@ -1,7 +1,6 @@
 package com.example.lucas_vieira.knowqui.view;
 
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,9 +8,12 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
+
 import com.example.lucas_vieira.knowqui.R;
 
 public class DadosCadastroActivity extends AppCompatActivity {
@@ -22,8 +24,8 @@ public class DadosCadastroActivity extends AppCompatActivity {
     private CheckBox checkBoxPublico;
     private CheckBox checkBoxPrivado;
 
-    private ImageButton imageButtonSexoFeminino;
-    private ImageButton imageButtonSexoMasculino;
+    private ImageView imageButtonSexoFeminino;
+    private ImageView imageButtonSexoMasculino;
 
     private Button botaoSalvar;
 
@@ -42,16 +44,26 @@ public class DadosCadastroActivity extends AppCompatActivity {
         botaoSalvar = findViewById(R.id.botaoSalvar);
         botaoSalvar.setOnClickListener(onClickListenerSalvar());
 
+        editTextNome = findViewById(R.id.editTextNomeId);
+        editTextIdade = findViewById(R.id.editTextIdadeId);
+
+        checkBoxPrivado = findViewById(R.id.checkBoxPrivadoId);
+        checkBoxPublico = findViewById(R.id.checkBoxPublicoId);
+
     }
+
 
     private View.OnClickListener onClickListenerSalvar() {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //todo JP FAZER
-                boolean camposValidados = validarCampos();
+                boolean campoNomeValidado = validarCampoNome();
+                boolean campoIdadeValidade = validarCampoIdade();
 
-                if(camposValidados){
+                if(campoNomeValidado == true){
+                    chamarTelaInicial();
+                }else if(campoIdadeValidade == true){
                     chamarTelaInicial();
                 }
 
@@ -60,9 +72,23 @@ public class DadosCadastroActivity extends AppCompatActivity {
         };
     }
 
+    private boolean validarCampoIdade() {
 
-    private boolean validarCampos() {
-        return true;
+        if(editTextIdade.getText().toString().equals("")){
+
+            Toast.makeText(getApplication(),"Campo Idade Vazio", Toast.LENGTH_SHORT).show();
+            editTextIdade.requestFocus();
+            return false;
+        }return  true;
+
+    }
+
+    private boolean validarCampoNome() {
+        if(editTextNome.getText().toString().equals("")){
+            Toast.makeText(getApplication(),"Campo Nome Vazio", Toast.LENGTH_SHORT).show();
+            editTextNome.requestFocus();
+            return false;
+        }return true;
     }
 
     private void chamarTelaInicial() {
@@ -70,6 +96,7 @@ public class DadosCadastroActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
 
 
 }
